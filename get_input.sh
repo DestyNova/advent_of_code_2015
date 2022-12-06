e31dfebe0af5f05825a64c1c33d8d36c2a356a85
Oisín Mac Fhearaí <denpashogai@gmail.com>
2022-12-06 20:40:15 +0000

    Rename dirs

--- get_input.sh
+++ get_input.sh
@@ -1,2 +1,5 @@
-mv ~/Downloads/input input.txt
-xsel -b > sample.txt
+#!/usr/bin/env bash
+
+DAY=$(basename $(pwd))
+
+curl --cookie "session=$(<../../session.txt)" https://adventofcode.com/2022/day/$DAY/input > input
